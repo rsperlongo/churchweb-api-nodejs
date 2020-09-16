45c6a9e8f7a5c64b876f73dbb664518937b6ba93
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2020-09-16 18:29:38 -0300

    added model and roles to authentication

--- app.js
+++ app.js
@@ -20,16 +20,61 @@ const eventsRouter = require('./routes/events');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const db = require('./models');
+const { role } = require('./models');
+const { count } = require('console');
+const Role = db.role;
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/members', membersRouter);
 app.use('/events', eventsRouter);
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if(!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if(err) {
+          console.log("err", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if(err) {
+          console.log("error", err);
+        }
+
+          console.log("added 'moderator' to collections")
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if(err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to collections")
+      });
+    }
+  });
+}
 
 module.exports = app;
